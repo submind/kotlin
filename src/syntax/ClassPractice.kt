1fa4a76bc2e5e4691fdbbcd0a1ebe39b2fbd7948
submind <submindwang@gmail.com>
2017-12-16 01:12:25 -0500

    abstract class

--- src/syntax/ClassPractice.kt
+++ src/syntax/ClassPractice.kt
@@ -7,7 +7,20 @@ open class Food (open val name: String, open var weight: Double) {
 
 class Onion(override val name: String = "Onion", override var weight: Double, val color: String): Food(name, weight)
 
+abstract class Person(open val name: String) {
+    abstract fun speak()
+}
+
+class Teacher(override val name: String): Person(name) {
+    override fun speak() {
+        println("Hi, I'm a teacher")
+    }
+
+}
+
 fun main(args: Array<String>) {
     val onion = Onion(weight = 12.1, color = "Red")
     onion.speak()
+    val teacher = Teacher("Lucy")
+    teacher.speak()
 }
